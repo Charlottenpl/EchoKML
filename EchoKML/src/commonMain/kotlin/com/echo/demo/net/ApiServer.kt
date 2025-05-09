16af13386fac616a7cbc7fed8f4228eb30827b59
陈重友 <33644882+Charlottenpl@users.noreply.github.com>
2025-05-09 19:09:57 +0800

    Update ApiServer.kt

--- EchoKML/src/commonMain/kotlin/com/echo/demo/net/ApiServer.kt
+++ EchoKML/src/commonMain/kotlin/com/echo/demo/net/ApiServer.kt
@@ -1,9 +1,22 @@
 package com.echo.demo.net
 
+import com.echo.demo.net.bean.InitBean
+import com.echo.demo.net.bean.NetCallback
+import com.echo.demo.net.bean.NetResult
+
 class ApiServer {
     companion object{
         public fun test(){
-            Net.get("")
+            val callback = object : NetCallback<InitBean> {
+                override fun onSuccess(data: NetResult<InitBean>) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onFail() {
+                    TODO("Not yet implemented")
+                }
+            }
+            Net.get(null, callback)
         }
     }
 }
